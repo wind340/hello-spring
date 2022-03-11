f8ffada0eec0af9ca9063e78d34b8cfc918c1f2f
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-03-11 23:49:30 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
-    Optional<Member> findByName(String name);
+    @Override
+    Optional<Member>
+    findByName(String name);
 }
